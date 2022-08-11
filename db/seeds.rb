07cb8bc0002c531801c296044d142be19fa953bf
Anais Roger <anais.roger@mail.mcgill.ca>
2022-08-11 14:30:15 -0400

    created a seed with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+# seed some movies
+puts 'Creating 20 fake movies...'
+
+Movie.destroy_all
+
+20.times do
+  movie = Movie.new(
+    title: Faker::Name.name,
+    overview: Faker::Quotes::Shakespeare.hamlet_quote,
+    rating:  (rand(0.1..5.0).round(2)),
+    poster_url: Faker::Internet.url)
+  movie.save!
+end
+
+puts "Finished! Created #{Movie.count} movies"
