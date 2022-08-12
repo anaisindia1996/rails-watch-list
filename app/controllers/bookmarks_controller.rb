5c9d04a3da4cf650e1386818e2317974225cc4a7
Anais Roger <anais.roger@mail.mcgill.ca>
2022-08-12 12:16:14 -0400

    added delete for bookmark and add a new list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,13 +7,25 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     @bookmark.save
     redirect_to list_path(@bookmark.list)
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:bookmark_id])
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
